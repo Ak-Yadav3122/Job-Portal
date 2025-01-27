776b2e42596a42197fcdb953938ce761895fd5e1
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-01-27 23:06:18 +0530

    fixe

--- frontend/src/utiles/constant.js
+++ frontend/src/utiles/constant.js
@@ -1,4 +1,4 @@
-const BackendURL = "https://job-portal-sy3g.vercel.app/";
+const BackendURL = "https://job-portal-sy3g.vercel.app";
 
 export const connectURL = `${BackendURL}/api/a1/user`;
 export const jobAPI = `${BackendURL}/api/a1/job`;
