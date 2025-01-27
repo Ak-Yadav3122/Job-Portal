39475fc449bb6076666e645a35b2bfd07bcc13e9
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-01-27 23:02:34 +0530

    updated url

--- frontend/src/utiles/constant.js
+++ frontend/src/utiles/constant.js
@@ -1,5 +1,6 @@
-export const connectURL = "https://job-portal-dont.onrender.com/api/a1/user";
-export const jobAPI = "https://job-portal-dont.onrender.com/api/a1/job";
-export const applicationAPI = "https://job-portal-dont.onrender.com/api/a1/application";
-export const comapnyAPI = "https://job-portal-dont.onrender.com/api/a1/company";
-// export const profilePhoto = "https://job-portal-dont.onrender.com/api/a1/profilePhoto";
+const BackendURL = "http://job-portal-sy3g.vercel.app/";
+
+export const connectURL = `${BackendURL}/api/a1/user`;
+export const jobAPI = `${BackendURL}/api/a1/job`;
+export const applicationAPI = `${BackendURL}/api/a1/application`;
+export const comapnyAPI = `${BackendURL}/api/a1/company`;
