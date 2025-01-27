fc904095ca88c40139d318a6113377b62bf159d6
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-01-27 22:24:34 +0530

    changes

--- frontend/src/utiles/constant.js
+++ frontend/src/utiles/constant.js
@@ -1,5 +1,5 @@
-export const connectURL = "http://localhost:5000/api/a1/user";
-export const jobAPI = "http://localhost:5000/api/a1/job";
-export const applicationAPI = "http://localhost:5000/api/a1/application";
-export const comapnyAPI = "http://localhost:5000/api/a1/company";
-// export const profilePhoto = "http://localhost:5000/api/a1/profilePhoto";
+export const connectURL = "https://job-portal-dont.onrender.com/api/a1/user";
+export const jobAPI = "https://job-portal-dont.onrender.com/api/a1/job";
+export const applicationAPI = "https://job-portal-dont.onrender.com/api/a1/application";
+export const comapnyAPI = "https://job-portal-dont.onrender.com/api/a1/company";
+// export const profilePhoto = "https://job-portal-dont.onrender.com/api/a1/profilePhoto";
