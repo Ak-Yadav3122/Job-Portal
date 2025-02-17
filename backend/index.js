c125e0e8b07489aaa132a539cc55e47a7ea9d408
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-02-17 23:06:58 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.use(cors({
 
 app.use(function (req, res, next) {
   //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://job-portal-xi-six.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     next();
