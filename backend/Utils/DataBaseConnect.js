fda37347c55b3b6234387bc5222707eeb64d0adf
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-02-26 20:14:54 +0530

    update

--- backend/Utils/DataBaseConnect.js
+++ backend/Utils/DataBaseConnect.js
@@ -4,6 +4,9 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
       dbName: "Job_Portal",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, 
     });
   } catch (error) {
     console.error("MongoDB connection error:", error);
