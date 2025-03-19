8c94bae6ea0bc345dd988e9b350ee0cecc703b85
Aichchhik Kumar <144606935+Ak-Yadav3122@users.noreply.github.com>
2025-03-19 22:18:31 +0530

    update the cors headers

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,8 @@ app.use(cors({
   origin: 'https://job-portal.aichchhik.xyz',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Credentials"],
+   exposedHeaders: ["Access-Control-Allow-Credentials"],
 }));
 
 //Creation of API'S
