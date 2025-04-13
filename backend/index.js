5398c9a0dfb1bbc618717738c25d701069e9edd1
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-04-13 23:24:37 +0530

    update the allowed origins

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
 "http://localhost:5173",
-"https://job-portal-f-virid.vercel.app/"
+"https://job-portal-f-virid.vercel.app"
 
 ];
 app.use(cors({
